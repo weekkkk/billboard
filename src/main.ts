644ee03ccee8b70f270790a9932e237f6e62c43f
KossaDenis <90174245+KossaDenis@users.noreply.github.com>
2022-10-31 22:06:49 +0300

    indent

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import "./assets/style/color/index.scss"
+import "./assets/style/indent/index.scss"
 
 const app = createApp(App)
 
